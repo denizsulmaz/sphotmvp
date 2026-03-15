18a4557845a82664ae0834a68291568dc92baf0e
Deniz Sulmaz <denizsulmaz@192.168.1.142>
2026-03-15 16:57:46 +0300

    Remove basePath for custom domain booksphot.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const basePath = isGithubActions ? '/sphotmvp' : '';
 
 const nextConfig = {
-  basePath: basePath,
-  assetPrefix: basePath + '/',
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
   output: "export",
   images: {
     unoptimized: true,
